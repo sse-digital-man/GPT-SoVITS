a889a356c20f732888f230efe9a71525dfadcf0f
kingki663 <1581690775@qq.com>
2024-05-13 21:22:13 +0800

    modify:适配submodule

--- GPT_SoVITS/my_utils.py
+++ GPT_SoVITS/my_utils.py
@@ -1,6 +1,6 @@
 import ffmpeg
 import numpy as np
-
+import os
 
 def load_audio(file, sr):
     try:
@@ -9,13 +9,22 @@ def load_audio(file, sr):
         # Requires the ffmpeg CLI and `ffmpeg-python` package to be installed.
         file = (
             file.strip(" ").strip('"').strip("\n").strip('"').strip(" ")
-        )  # 防止小白拷路径头尾带了空格和"和回车
+        )  # 防止小白拷路径头尾带了空格和"和回车'
+        # print("filename:", file)
+        # filepath =  os.path.join(os.getcwd(), file)
+        # print("filepath:", filepath)
+        # if os.path.exists(filepath):
+        #     print("文件存在")
+        # else:
+        #     print("文件不存在")
         out, _ = (
             ffmpeg.input(file, threads=0)
             .output("-", format="f32le", acodec="pcm_f32le", ac=1, ar=sr)
             .run(cmd=["ffmpeg", "-nostdin"], capture_stdout=True, capture_stderr=True)
         )
+
     except Exception as e:
-        raise RuntimeError(f"Failed to load audio: {e}")
+        # raise RuntimeError(f"Failed to load audio: {e}")
+        raise e
 
     return np.frombuffer(out, np.float32).flatten()
